8e526ba7bb32f486122aad0b0d80d39fd84fa1fe
Piotr <P.Larkowski90@gmail.com>
2024-03-24 17:13:07 +0100

    Add first method showing description of players by using switch and array

--- src/FootballMatchReports.java
+++ src/FootballMatchReports.java
@@ -1,3 +1,17 @@
 public class FootballMatchReports {
-
+    public static String onField(int shirtNum) {
+        String[] playerDescriptions = {"goalie","left back","center back","right back","midfielder","left wing","striker","right wing"};
+        String returnString = "";
+        switch (shirtNum) {
+            case 1 -> returnString = playerDescriptions[0];
+            case 2 -> returnString = playerDescriptions[1];
+            case 3, 4 -> returnString = playerDescriptions[2];
+            case 5 -> returnString = playerDescriptions[3];
+            case 6, 7, 8 -> returnString = playerDescriptions[4];
+            case 9 -> returnString = playerDescriptions[5];
+            case 10 -> returnString = playerDescriptions[6];
+            case 11 -> returnString = playerDescriptions[7];
+        }
+        return returnString;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Player descriptions:");
+        System.out.println("Player one - " + FootballMatchReports.onField(1));
+        System.out.println("Player five - " + FootballMatchReports.onField(5));
+        System.out.println("Player ten - " + FootballMatchReports.onField(10));
     }
 }
